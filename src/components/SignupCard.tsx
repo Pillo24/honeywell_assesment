43af0aa73fe15032f29ef0733b3fb7c35b7f2886
MohammedKhalidJ <mohammedkhalid11@gmail.com>
2022-08-29 00:19:36 -0500

    feat: validation implementation

--- src/components/SignupCard.tsx
+++ src/components/SignupCard.tsx
@@ -13,16 +13,35 @@ import {
   useColorModeValue,
   InputLeftElement,
   chakra,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { EmailIcon, ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
 import { FaUserAlt, FaLock } from "react-icons/fa";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { SignupSubmitForm } from "./types";
+import { validationSchema } from "../schema/signupCardSchema";
 
 const CFaUserAlt = chakra(FaUserAlt);
 const CFaLock = chakra(FaLock);
 
 export default function SignupCard() {
   const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showConfirmPassword, setShowConfirmPassword] =
+    useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupSubmitForm>({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data: SignupSubmitForm) => {
+    console.log(JSON.stringify(data, null, 2));
+  };
 
   return (
     <Flex
@@ -43,46 +62,72 @@ export default function SignupCard() {
           boxShadow={"lg"}
           p={8}
         >
-          <form>
+          <form id="signupForm" onSubmit={handleSubmit(onSubmit)}>
             <Stack spacing={4}>
               <HStack display={{ sm: "none", md: "flex" }}>
                 <Box>
-                  <FormControl id="firstName" isRequired>
+                  <FormControl
+                    id="firstName"
+                    isInvalid={errors.firstname?.message ? true : false}
+                    isRequired
+                  >
                     <FormLabel>First Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text" {...register("firstname")} />
+                    <FormErrorMessage>
+                      {errors.firstname?.message}
+                    </FormErrorMessage>
                   </FormControl>
                 </Box>
                 <Box>
                   <FormControl id="lastName">
                     <FormLabel>Last Name</FormLabel>
-                    <Input type="text" />
+                    <Input type="text" {...register("lastname")} />
                   </FormControl>
                 </Box>
               </HStack>
-              <FormControl id="userName" isRequired>
+              <FormControl
+                id="userName"
+                isInvalid={errors.username?.message ? true : false}
+                isRequired
+              >
                 <FormLabel>Username</FormLabel>
                 <InputGroup>
                   <InputLeftElement
                     pointerEvents="none"
                     children={<CFaUserAlt color="gray.300" />}
                   />
-                  <Input type="text" />
+                  <Input type="text" {...register("username")} />
                 </InputGroup>
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
               </FormControl>
-              <FormControl id="email" isRequired>
+              <FormControl
+                id="email"
+                isInvalid={errors.email?.message ? true : false}
+                isRequired
+              >
                 <FormLabel>Email address</FormLabel>
                 <InputGroup>
                   <InputLeftElement
                     pointerEvents="none"
                     children={<EmailIcon color="gray.300" />}
                   />
-                  <Input type="email" />
+                  <Input type="email" {...register("email")} />
                 </InputGroup>
+                <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
               </FormControl>
-              <FormControl id="password" isRequired>
+              <FormControl
+                id="password"
+                isInvalid={errors.password?.message ? true : false}
+                isRequired
+              >
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                  <Input type={showPassword ? "text" : "password"} />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    {...register("password")}
+                  />
                   <InputLeftElement
                     pointerEvents="none"
                     children={<CFaLock color="gray.300" />}
@@ -98,11 +143,19 @@ export default function SignupCard() {
                     </Button>
                   </InputRightElement>
                 </InputGroup>
+                <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
               </FormControl>
-              <FormControl id="confirmPassword" isRequired>
+              <FormControl
+                id="confirmPassword"
+                isInvalid={errors.confirmPassword?.message ? true : false}
+                isRequired
+              >
                 <FormLabel>Confirm Password</FormLabel>
                 <InputGroup>
-                  <Input type={showPassword ? "text" : "password"} />
+                  <Input
+                    type={showConfirmPassword ? "text" : "password"}
+                    {...register("confirmPassword")}
+                  />
                   <InputLeftElement
                     pointerEvents="none"
                     children={<CFaLock color="gray.300" />}
@@ -111,16 +164,22 @@ export default function SignupCard() {
                     <Button
                       variant={"ghost"}
                       onClick={() =>
-                        setShowPassword((showPassword) => !showPassword)
+                        setShowConfirmPassword(
+                          (showConfirmPassword) => !showConfirmPassword
+                        )
                       }
                     >
-                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      {showConfirmPassword ? <ViewIcon /> : <ViewOffIcon />}
                     </Button>
                   </InputRightElement>
                 </InputGroup>
+                <FormErrorMessage>
+                  {errors.confirmPassword?.message}
+                </FormErrorMessage>
               </FormControl>
               <Stack spacing={10} pt={2}>
                 <Button
+                  type="submit"
                   loadingText="Submitting"
                   size="lg"
                   bg={"blue.400"}
