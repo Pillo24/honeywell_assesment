631712639237376958acbe3f0adda88441fcd21b
Manuel Mendoza <juamendo@publicisgroupe.net>
2022-08-30 15:31:44 -0500

    Add: Basic Form

--- src/pages/singup/singup.tsx
+++ src/pages/singup/singup.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../components/button';
+import Input from '../../controls/input';
 
 // Styles
 import './singup.scss';
@@ -8,9 +10,53 @@ export interface SingUpProps {
 }
 
 const SingUp: React.FC = (): JSX.Element => {
+    const [userName, setUserName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [validation, setValidation] = useState<string>('');
+
+    const handleSubmit = (e: React.FormEvent): void => {
+        e.preventDefault();
+        const user = { userName, email, password };
+        console.log('user:', user);
+    }
+
     return (
         <div className='sing-up__container'>
-            Hello
+            <form className='sing-up__form' onSubmit={handleSubmit}>
+                <Input
+                    fieldClassName="sing-up__form-field"
+                    required
+                    labelText='Username'
+                    value={userName}
+                    setValue={setUserName}
+                />
+                <Input
+                    fieldClassName="sing-up__form-field"
+                    required
+                    labelText='Email'
+                    value={email}
+                    setValue={setEmail}
+                />
+                <Input
+                    fieldClassName="sing-up__form-field"
+                    required
+                    type="password"
+                    labelText='Password'
+                    value={password}
+                    setValue={setPassword}
+                />
+                <Input
+                    fieldClassName="sing-up__form-field"
+                    required
+                    type="password"
+                    labelText='Password Validation'
+                    helper='Plase confirm password'
+                    value={validation}
+                    setValue={setValidation}
+                />
+                <Button>Sign Up</Button>
+            </form>
         </div>
     )
 };
