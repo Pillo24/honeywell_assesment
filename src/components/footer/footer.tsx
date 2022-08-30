61631b752ae558cde940b8471a423de5292c01b5
Manuel Mendoza <juamendo@publicisgroupe.net>
2022-08-29 23:48:55 -0500

    Add: page footer

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Styles
+import './footer.scss';
+
+
+const Footer: React.FC = (): JSX.Element => (
+    <footer className='footer__container'>
+        <p>All rights reserved Manuel Mendoza</p>
+    </footer>
+);
+
+export default Footer;
