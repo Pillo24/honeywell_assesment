517fe0d9977a2d4f07a70f56dd3f63c62abcf6ec
Miguel <maikmajestic@icloud.com>
2022-09-02 10:27:07 -0500

    feat: Added project settings
    Added dependencies and settings for the project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>Hello there #FutureShaper...</h3>
-        <p>
-          This is a simple React app that will be used to asses your skills.
-        </p>
-        <p>
-          Please follow the instructions in the README.md file to get started.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and show me what you got!
-        </p>
-      </header>
+      <div>Hello!</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
