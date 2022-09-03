a59a6fff5430003b62321d878794807209ae920f
Oscar Ramo <oscar.ramo1289@gmail.com>
2022-09-03 15:49:10 -0500

    Add validation to context and Add two dumbd comps

--- front/src/hooks/useUserAuthContext.tsx
+++ front/src/hooks/useUserAuthContext.tsx
@@ -3,6 +3,11 @@ import { UserAuthContext } from '../context/UserAuthContext'
 
 export const useUserAuthContext = () => {
     const context = useContext(UserAuthContext)
-
+    //just in case something fails
+    if (!context) {
+        throw Error(
+            'UserAuthContext must be used inside UserAuthContextProvider'
+        )
+    }
     return context
 }
