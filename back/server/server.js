8ad7b581514b54373f10781cd08c5d68878ea359
Oscar Ramo <oscar.ramo1289@gmail.com>
2022-09-03 15:49:56 -0500

    Fix some problems with conection with the db

--- back/server/server.js
+++ back/server/server.js
@@ -11,27 +11,29 @@ const password = '1GoZPeuoppIotaLF'
 const cluster = 'my-first-cluster'
 const dbname = 'dbOscar'
 
-const dataBase = (module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
-    try {
-        mongoose.connect(
-            `mongodb+srv://${dbname}:${password}@${cluster}.bivp5.mongodb.net/?retryWrites=true&w=majority`,
-            connectionParams
-        )
-        console.log('Connected successfully')
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-dataBase()
-
 //routes
 app.use('/api/user', userRouter)
 
-app.listen(3000, () => {
-    console.log('Server is running at port 3000')
-})
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+mongoose
+    .connect(
+        `mongodb+srv://${dbname}:${password}@${cluster}.bivp5.mongodb.net/?retryWrites=true&w=majority`,
+        connectionParams
+    )
+    .then(() => {
+        // listen for requests port 3000
+        app.listen(3000, () => {
+            console.log('connected to db & listening on port', 3000)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+// app.listen(4000, () => {
+//     console.log('Server is running at port 4000')
+// })
