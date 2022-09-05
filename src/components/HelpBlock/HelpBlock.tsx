6e0cbed37807e288af0cd1638527cdbe4bea9648
Laracats <luis.barajas.dev@outlook.com>
2022-09-05 00:29:25 -0500

    refactor: added helpblock for inputs

---
+++ src/components/HelpBlock/HelpBlock.tsx
@@ -0,0 +1,55 @@
+import React, { FC, HTMLAttributes, useState, useRef } from 'react';
+import styled from 'styled-components';
+import { AiFillInfoCircle } from 'react-icons/ai';
+
+const HelpBlockWrapper = styled.div`
+display: inline-block;
+position: relative;
+`;
+
+const HelpBlockContent = styled.div`
+position: absolute;
+min-width: 200px;
+background: #fff;
+top: 20px;
+left: 0;
+transform: translate(-50%, 0);
+border: solid #dee2e6;
+border-radius: 4px;
+padding: 8px;
+`;
+
+const HelpBlockButton = styled.button`
+background: transparent;
+border: none;
+`;
+
+export interface HelpBlockProps extends HTMLAttributes<HTMLDivElement> {
+    id: string;
+}
+
+export const HelpBlock: FC<HelpBlockProps> = ({
+    children,
+    id,
+    ...rest
+}) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+    const toggle = () => {
+       setIsOpen((opened) => !opened);
+    }
+
+    return (
+        <HelpBlockWrapper ref={wrapperRef} {...rest}>
+            <HelpBlockButton type="button" aria-label="Get help" onClick={toggle}>
+                <AiFillInfoCircle size={20} />
+            </HelpBlockButton>
+            {isOpen && (
+                <HelpBlockContent>
+                    {children}
+                </HelpBlockContent>
+            )}
+        </HelpBlockWrapper>
+    );
+}
