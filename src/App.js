dbf6c8d57cc071fbc322f79dd57a448c499de09a
tavoleglise <tavoleglise@gmail.com>
2022-09-06 07:40:24 -0500

    Validation logic implemented, first styles added

--- src/App.js
+++ src/App.js
@@ -17,6 +17,10 @@ function App() {
       type: "text",
       placeholder: "Username",
       label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      errMessage:
+        "Username should be 3-16 characters and should't include any special character",
+      required: true,
     },
     {
       id: 2,
@@ -24,6 +28,8 @@ function App() {
       type: "email",
       placeholder: "Email",
       label: "Email",
+      errMessage: "Enter a valid email address",
+      required: true,
     },
     {
       id: 3,
@@ -31,18 +37,26 @@ function App() {
       type: "password",
       placeholder: "Password",
       label: "Password",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      errMessage:
+        "Password should contain at least 1 letter, 1 number and 1 special character",
+      required: true,
     },
     {
-      id: 1,
+      id: 4,
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
       label: "Confirm Password",
+      errMessage: "Password don't match",
+      pattern: values.password,
+      required: true,
     },
   ];
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("paso");
   };
 
   const handleChange = (e) => {
@@ -51,20 +65,32 @@ function App() {
   };
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
-        {inputs.map((input) => (
-          <FormInput
-            key={input.id}
-            name={input.name}
-            placeholder={input.placeholder}
-            label={input.label}
-            type={input.type}
-            value={values[input.name]}
-            onChange={handleChange}
-          />
-        ))}
-        <button>Submit</button>
-      </form>
+      <div className="title">
+        <strong>Sing up</strong> form
+      </div>
+      <div className="box">
+        <div className="form_container">
+          <div className="form_container_title">Register</div>
+          <form onSubmit={handleSubmit}>
+            {inputs.map((input) => (
+              <FormInput
+                key={input.id}
+                name={input.name}
+                placeholder={input.placeholder}
+                label={input.label}
+                type={input.type}
+                value={values[input.name]}
+                onChange={handleChange}
+                pattern={input.pattern}
+                errMessage={input.errMessage}
+                required={input.required}
+              />
+            ))}
+            <button>Submit</button>
+          </form>
+        </div>
+        <div className="content_container">hola</div>
+      </div>
     </div>
   );
 }
--- src/components/FormInput/index.js
+++ src/components/FormInput/index.js
@@ -1,9 +1,28 @@
-export default function FormInput({ label, onChange, ...inputProps }) {
-  const handleChange = (e) => {};
+import { useState } from "react";
+import styles from "./FormInput.module.scss";
+
+export default function FormInput({
+  label,
+  onChange,
+  errMessage,
+  ...inputProps
+}) {
+  const [focused, setFocused] = useState(false);
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
-    <div>
+    <div className={styles.input_container}>
       <label htmlFor="">{label}</label>
-      <input {...inputProps} onChange={onChange} />
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <span>{errMessage}</span>
     </div>
   );
 }
